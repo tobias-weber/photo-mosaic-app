0c29abe6c9ab4b52d7c954c4a55942408bab22b6
Tobias Weber <tm.weber@stud.unibas.ch>
2025-09-08 13:43:29 +0200

    Setup project structure

---
+++ frontend/src/app/components/endpoint-tester/endpoint-tester.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-endpoint-tester',
+    imports: [],
+    templateUrl: './endpoint-tester.html',
+    styleUrl: './endpoint-tester.css'
+})
+export class EndpointTester {
+
+}
